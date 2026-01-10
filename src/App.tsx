2b15b68eae2364ead3fb05aaa0216bee94cc319e
Lajos Nagy <lajos.j.nagy@gmail.com>
2026-01-10 12:15:46 +0100

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import Navbar from "./components/layout/Navbar";
 import Home from "./pages/Home";
 
-function App() {
-    return <Home />;
+export default function App() {
+    return (
+        <>
+            <Navbar />
+            <Home />
+        </>
+    );
 }
-
-export default App;
